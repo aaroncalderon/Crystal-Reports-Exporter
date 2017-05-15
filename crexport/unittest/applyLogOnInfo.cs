45208ed1080c50b89c513737a5229f7acd444cc5
aaroncalderon <aaroncalderon@users.noreply.github.com>
2017-05-15 10:16:22 -0500

    WIP removed `applyLogOnInfo.cs` from compile list.

--- crexport/unittest/applyLogOnInfo.cs
+++ crexport/unittest/applyLogOnInfo.cs
@@ -1,4 +1,8 @@
-﻿        /// <source>
+﻿namespace applyLogOnInfo
+{
+    class aplylogoninfo
+    {
+        /// <source>
         /// This code came from
         /// https://archive.sap.com/discussions/thread/1891114
         /// </source>
@@ -11,20 +15,20 @@
         private void ApplyLoginInfo(ReportDocument Report, Report rptinfo)
         {
             TableLogOnInfo logonInfo = null;
- 
+
             try
             {
                 #region Credentials
- 
+
                 //
                 // Define credentials
                 //
                 logonInfo = new TableLogOnInfo();
- 
-                logonInfo.ConnectionInfo.AllowCustomConnection = true;                
+
+                logonInfo.ConnectionInfo.AllowCustomConnection = true;
                 logonInfo.ConnectionInfo.ServerName = rptinfo.Server;
-                logonInfo.ConnectionInfo.DatabaseName = rptinfo.Database;                
- 
+                logonInfo.ConnectionInfo.DatabaseName = rptinfo.Database;
+
                 //
                 // Set the userid/password for the rptinfo if we are not using integrated security
                 //
@@ -37,11 +41,11 @@
                     logonInfo.ConnectionInfo.Password = rptinfo.Password;
                     logonInfo.ConnectionInfo.UserID = rptinfo.UserId;
                 }
- 
+
                 #endregion
- 
+
                 #region Apply to connections, tables and sub-reports
- 
+
                 //
                 // Main connection?
                 //
@@ -50,18 +54,18 @@
                     logonInfo.ConnectionInfo.ServerName,
                     logonInfo.ConnectionInfo.DatabaseName,
                     false);
- 
+
                 //
                 // Other connections?
                 //
                 foreach (CrystalDecisions.Shared.IConnectionInfo connection in Report.DataSourceConnections)
-                {                    
+                {
                     connection.SetConnection(rptinfo.Server, rptinfo.Database, rptinfo.IntegratedSecurity);
                     connection.SetLogon(rptinfo.UserId, rptinfo.Password);
                     connection.LogonProperties.Set("Data Source", rptinfo.Server);
                     connection.LogonProperties.Set("Initial Catalog", rptinfo.Database);
                 }
- 
+
                 //
                 // Only do this to the main rptinfo (can't do it to sub reports)
                 //
@@ -72,27 +76,27 @@
                     //                
                     foreach (ReportDocument rd in Report.Subreports)
                     {
-                        ApplyLoginInfo(rd, rptinfo);                        
+                        ApplyLoginInfo(rd, rptinfo);
                     }
                 }
- 
+
                 //
                 // Apply to tables
                 //
                 foreach (CrystalDecisions.CrystalReports.Engine.Table table in Report.Database.Tables)
                 {
                     TableLogOnInfo tableLogOnInfo = table.LogOnInfo;
-                                        
-                    tableLogOnInfo.ConnectionInfo = logonInfo.ConnectionInfo;                    
+
+                    tableLogOnInfo.ConnectionInfo = logonInfo.ConnectionInfo;
                     table.ApplyLogOnInfo(tableLogOnInfo);
                     if (!table.TestConnectivity())
                     {
                         Debug.WriteLine("Failed to apply log in logonInfo for Crystal Report");
                     }
                 }
- 
+
                 #endregion
- 
+
                 try
                 {
                     //
@@ -111,3 +115,5 @@
                     ex.Message);
             }
         }
+    }
+}
